47d807983c715dcc3c42c09820f0a33c6a816279
Mack <42896437+Mackgame4@users.noreply.github.com>
2025-04-23 19:52:14 +0100

    Started exerc2

--- ENGWEB2025-Afericao/ex2/routes/index.js
+++ ENGWEB2025-Afericao/ex2/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get('http://localhost:17000/books').then(function (response) {
+    // handle success
+    console.log(response.data);
+    res.render('index', { title: 'ApiLivros', livros: response.data.items });
+  }).catch(function (error) {
+    // handle error
+    console.log(error);
+    res.render('index', { title: 'ApiLivros', livros: [] });
+  });
 });
 
 module.exports = router;
